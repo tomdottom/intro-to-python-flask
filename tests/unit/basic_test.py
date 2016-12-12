e378f9befa469e0d3b1dd3cfa72973d09b364e04
Tom Marks <thomas.o.marks@gmail.com>
2016-12-12 18:49:48 -0500

    Improves readability of test example

--- tests/unit/basic_test.py
+++ tests/unit/basic_test.py
@@ -2,34 +2,35 @@ import os
 import unittest
 import tempfile
 
-import app
+from app import app, db
 from init_db import init_db
 
 
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.test_client = app.test_client()
+        self._create_tmp_db()
+        with app.app_context():
+            self._initalise_db(db)
+
+    def _create_tmp_db(self):
         self.db_fd, self.tmp_filename = tempfile.mkstemp()
-        app.app.config['SQLALCHEMY_DATABASE_URI'] = (
+        app.config['SQLALCHEMY_DATABASE_URI'] = (
             'sqlite:///' + self.tmp_filename)
-        app.app.config['TESTING'] = True
-        self.app = app.app.test_client()
-        with app.app.app_context():
-            app.db.create_all()
+
+    def _initalise_db(self, db):
+        db.create_all()
+        init_db(db)
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(self.tmp_filename)
 
     def test_index_renders_member(self):
-        with app.app.app_context():
-            init_db(app.db)
 
-        response = self.app.get('/')
+        response = self.test_client.get('/')
 
         assert 200 == response.status_code
         assert 'Tom' in response.data
         assert 'Matt' in response.data
-
-if __name__ == '__main__':
-    unittest.main()
